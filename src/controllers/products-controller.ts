428243c9e0a98c382265654a3c1784c921f1ca07
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-21 10:42:28 -0300

    add middleware to exception handling

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError.js";
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction){
         try {
-
+            
             return response.json({ message: "Ok" });
         } catch(error) {
             next(error);
