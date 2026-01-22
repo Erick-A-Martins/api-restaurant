78074d79e70dd17a8b8a3bab0f7bc2e95c11c141
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-22 09:48:31 -0300

    add validation for open tables sessions

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError.js";
 import { db } from "@/database/knex.js";
 import { z } from "zod";
 
@@ -10,6 +11,15 @@ class TablesSessionsController {
             });
 
             const { table_id } = bodySchema.parse(request.body);
+
+            const session = await db<TablesSessionsRepository>("tables_sessions")
+                .where({ table_id })
+                .orderBy("opened_at", "desc")
+                .first();
+
+            if(session && !session.closed_at) {
+                throw new AppError("this table is already open");
+            }
 
             await db<TablesSessionsRepository>("tables_sessions").insert({
                 table_id,
