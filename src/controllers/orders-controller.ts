e41b138c0dd1c687d6165f8b28b3713934003dae
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-24 08:54:06 -0300

    add insert method to orders

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -37,7 +37,14 @@ class OrdersController {
                 throw new AppError("product not found");
             }
 
-            return response.status(201).json(session);
+            await db<OrderRepository>("orders").insert({
+                table_session_id,
+                product_id,
+                quantity,
+                price: product.price
+            });
+
+            return response.status(201).json();
         } catch (error) {
             next(error);
         }
